6eb6d7a4410da5ac636802f6db7db431d6d98187
DeerLuuu <17359672787@163.com>
2023-10-13 00:12:37 +0800

    make player moved and animation

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,28 +1,43 @@
 extends CharacterBody2D
 
+@export var spped : float = 300
+var player_animation
+var player_sprite
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready() -> void:
+	# 我用它来进行初始化
+	player_animation = $AnimationPlayer
+	player_sprite = $Sprite2D
+	animation_switch("idle")
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delta) -> void:
+	var movement_vector_normalized: Vector2 = get_movement_vector_normalized()
+	
+	velocity = movement_vector_normalized * spped
+	
+	if velocity != Vector2.ZERO:
+		set_scale_inverse()
+		animation_switch("run")
+		move_and_slide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation_switch("idle")
 
-	move_and_slide()
+func get_movement_vector_normalized() -> Vector2:
+	var x_movement : float = Input.get_action_strength("action_right") - Input.get_action_strength("action_left")
+	var y_movement : float = Input.get_action_strength("action_down") - Input.get_action_strength("action_up")
+	
+	return Vector2(x_movement, y_movement).normalized()
+
+func animation_switch(animation : String) -> void:
+	player_animation.play(animation)
+
+func set_scale_inverse() -> void:
+	# 角色方向取反,测试了一下，修改scale的值会出现鬼畜的错误，原因未知
+	# 最后只能注入Sprite2D然后修改Sprite2D中的flip_h
+	
+	if get_movement_vector_normalized().x > 0:
+		# scale.x = 1
+		player_sprite.flip_h = false
+	elif get_movement_vector_normalized().x < 0:
+		# scale.x = -1
+		player_sprite.flip_h = true
